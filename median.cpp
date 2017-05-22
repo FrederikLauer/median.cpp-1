e77c8f2d6fbdbefff0d357fef9d38c41613e97fb
frederiklauer <frederiklauer@me.com>
2017-05-22 21:42:44 +0200

    Median Filter implemented

--- median.cpp
+++ median.cpp
@@ -8,11 +8,24 @@
 // Christian De Schryver <schryver@eit.uni-kl.de>
 
 #include<image.h>
+#include <vector>
+using namespace std;
 
 void medianFilter(const unsigned char input[][HEIGHT], unsigned char output[][HEIGHT], unsigned int width, unsigned int height)
 {
+    for(unsigned int output_w = 1; output_w < width-1; output_w++){
+        for(unsigned int output_h = 1 ; output_h < height-1; output_h++){
+        vector<unsigned char> tmp;
+            for(int w = -1 ; w < 2; w++){
+                for(int h = -1 ; h < 2; h++){
+                    tmp.push_back(input[output_w-w][output_h-h]);
+                }
+            }
+        std::sort (tmp.begin(), tmp.end());
+        output[output_w][output_h] = tmp.at(5);
+        }
+    }
 
-    //TODO: put your implementation here
 
 }
 
